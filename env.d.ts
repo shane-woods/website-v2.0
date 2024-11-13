a9f30e6d4fb7698972912d93ad130e5d562c0524
Shane <shwoods35@gmail.com>
2024-11-13 14:17:16 -0500

    added run to scripts

--- env.d.ts
+++ env.d.ts
@@ -2,8 +2,5 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_SUPABASE_URL: string;
     NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
-    NEXT_PUBLIC_SPOTIFY_CLIENT_ID: string;
-    NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET: string;
-    NEXT_PUBLIC_SPOTIFY_REFRESH_TOKEN: string;
   }
 }
--- next.config.js
+++ next.config.js
@@ -29,6 +29,11 @@ const nextConfig = {
     return config;
   },
 
+  env: {
+    SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+
   images: {
     remotePatterns: [
       {
