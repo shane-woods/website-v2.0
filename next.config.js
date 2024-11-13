246ba6eb617ed9fe85da55444a112c3d7591d7bb
Shane <shwoods35@gmail.com>
2024-11-13 14:25:17 -0500

    added run to scripts

--- next.config.js
+++ next.config.js
@@ -29,11 +29,6 @@ const nextConfig = {
     return config;
   },
 
-  env: {
-    SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
-
   images: {
     remotePatterns: [
       {
